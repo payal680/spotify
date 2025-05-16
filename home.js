c600c98abe8c56aa2d2c4f87992979e0a07d1d96
payal680 <patwariyapayal82@gmail.com>
2025-05-16 18:00:01 +0530

    changes in js

--- home.js
+++ home.js
@@ -1,10 +1,1 @@
 console.log("lets write java script")
-async function main(){
-    let song = await fetch("https://github.com/payal680/spotify/tree/main/songsfolder")
-    let responce = await song.text()
-    console.log(responce)
-    let div = document.createElement('div')
-        div.innerHTML = responce;
-        let tds= div.getElementsByTagName("td")
-}
-
