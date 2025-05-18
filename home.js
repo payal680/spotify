8bd1ec82970f5417d3be2177db0e01b0e7d23ea6
payal680 <patwariyapayal82@gmail.com>
2025-05-18 12:53:44 +0530

    songs name change

--- home.js
+++ home.js
@@ -1,1 +1,12 @@
-console.log("lets write java script")
+let songsfolder = ["spotify\songsfolder\128-Ishq Sufiyana (Male) - The Dirty Picture 128 Kbps.mp3","spotify\songsfolder\128-Mere Bina - Crook 128 Kbps.mp3"
+  
+];
+function playSong(index) {
+  const audioPlayer = document.getElementById('audioPlayer1');
+  if (audioPlayer) {
+    audioPlayer.src = songsfolder[index];
+    audioPlayer.play();
+  } else {
+    console.error("Audio player element not found.");
+  }
+}
